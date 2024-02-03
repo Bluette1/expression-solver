fa28c14f531dcc9a2d7f0a7d282e21d2d87eff54
Bluette1 <marylene.sawyer@gmail.com>
2024-02-03 08:34:46 +0200

    Clean up, Error Handling

--- expression_solver.py
+++ expression_solver.py
@@ -7,22 +7,22 @@ To solve algebraic expressions, we'll take the following steps
 '''
 
 
-def evaluate_tree(root):
+def evaluate_expression_tree(root):
     if (root.value.isnumeric()):
         return root.value
-    left_side = evaluate_tree(root.left_child)
-    right_side = evaluate_tree(root.right_child)
+    left_side = evaluate_expression_tree(root.left_child)
+    right_side = evaluate_expression_tree(root.right_child)
 
     return int(evaluate([left_side, right_side], root.value))
 
 
-def print_tree(root, str=[]):  # Inorder traversal gives infix expression
+def print_expression_tree(root, str=[]):  # Inorder traversal gives infix expression
     if (root != None):
         if root.left_child:
-            str = str + print_tree(root.left_child, [])
+            str = str + print_expression_tree(root.left_child, [])
         str += [root.value]
         if root.right_child:
-            str = str + print_tree(root.right_child, [])
+            str = str + print_expression_tree(root.right_child, [])
 
     return str
 
@@ -52,9 +52,37 @@ def build_expression_tree(expression):
     return stack[0]  # root of expression tree
 
 
-def convert_to_postfix(expression):
-    expression = ''.join(expression.split(' '))
+def evaluate(operands, operator):
 
+    if operator == '/':
+        return int(operands[0]) / int(operands[1])
+
+    elif operator == '*':
+        return int(operands[0]) * int(operands[1])
+
+    elif operator == '+':
+        return int(operands[0]) + int(operands[1])
+
+    elif operator == '-':
+        return int(operands[0]) - int(operands[1])
+
+
+def solve_postfix_using_stack(expression):
+    expression = expression.split(' ')
+    stack = []
+    for idx in range(len(expression)):
+        if ((expression[idx]).isnumeric()):
+            stack.append(expression[idx])
+        else:
+            operator = expression[idx]
+            operand_2 = stack.pop()
+            operand_1 = stack.pop()
+            solution = evaluate([operand_1, operand_2], operator)
+            stack.append(solution)
+    return int(stack[0])
+
+
+def convert_infix_to_postfix(expression):
     precedence_map = {
         "/": 2,
         '*': 2,
@@ -64,6 +92,9 @@ def convert_to_postfix(expression):
     postfix = []
     stack = []
     next = 0
+
+    # try:
+    expression = ''.join(expression.split(' '))
     for idx in range(len(expression)):
         if (next > idx):
             continue
@@ -72,7 +103,7 @@ def convert_to_postfix(expression):
             while(next < len(expression) and (expression[next]).isnumeric()):
                 next = next + 1
             postfix.append(expression[idx:next])  # add it to postfix
-        else:
+        else:  # is an operator
             current = expression[idx]
             if(len(stack) == 0):
                 stack.append(current)
@@ -95,62 +126,31 @@ def convert_to_postfix(expression):
     return postfix
 
 
-# postfix = convert_to_postfix('(4-1)')
-postfix = convert_to_postfix('(4 - 1) * (4 / (5 + 2) + 1)')
-# postfix = convert_to_postfix('12 * 3 / 12 + 3')
-# postfix = convert_to_postfix('(3 + 3) * 42 / (6 + 12)')
-# TREE 12 + 6 / 42 * 3 + 3
-# TREE (12 + 6) / 42 *(3 + 3)
-# TREE 3 + 3 * 42 / 6 + 12
+def evaluate_expression(expression):
 
-# postfix = convert_to_postfix('1*(2+3+4)')
-postfix = convert_to_postfix('3 + 12 * 3 / 12')
+    try:
+        postfix = convert_infix_to_postfix(expression)
+        
+        print('Postfix: ', postfix)
 
-print('Postfix: ', postfix)
+        print('SOLUTION STACK: ', solve_postfix_using_stack(' '.join(postfix)))
 
+        expression_tree_root = build_expression_tree(' '.join(postfix))
 
-def evaluate(operands, operator):
-
-    if operator == '/':
-        return int(operands[0]) / int(operands[1])
-
-    elif operator == '*':
-        return int(operands[0]) * int(operands[1])
-
-    elif operator == '+':
-        return int(operands[0]) + int(operands[1])
-
-    elif operator == '-':
-        return int(operands[0]) - int(operands[1])
+        # print('Expression Tree Infix: ', ' '.join(print_expression_tree(expression_tree_root, [])))
+        print('SOLUTION EXPRESSION TREE: ', evaluate_expression_tree(expression_tree_root))
+    except:
+        print('Invalid Expression',)
 
 
-def solve_expression(expression):
-    expression = expression.split(' ')
-    stack = []
-    for idx in range(len(expression)):
-        if ((expression[idx]).isnumeric()):
-            stack.append(expression[idx])
-        else:
-            operator = expression[idx]
-            operand_2 = stack.pop()
-            operand_1 = stack.pop()
-            solution = evaluate([operand_1, operand_2], operator)
-            stack.append(solution)
-    return int(stack[0])
+# evaluate_expression('(4-1)')
+# evaluate_expression('(4 - 1) * (4 / (5 + 2) + 1)')
+# evaluate_expression('12 * 3 / 12 + 3')
+# evaluate_expression('(3 + 3) * 42 / (6 + 12)')
+# evaluate_expression('1*(2+3+4)')
+evaluate_expression('3 + 12 * 3 / 12')
 
-
-# solved = solve_expression('4 1 - 4 5 2 + / 1 + *')
-
-# solved = solve_expression('3 12 + * 3 12 /')
-
-# solved = solve_expression('2 4 + 4 6 + *')
-
-# solved = solve_expression('3 3 + 42 * 6 12 + /')
-solved = solve_expression(' '.join(postfix))
-
-print('SOLUTION STACK: ', solved)
-
-expression_tree_root = build_expression_tree(' '.join(postfix))
-
-# print('TREE: ', ' '.join(print_tree(expression_tree_root, [])))
-print('SOLUTION TREE: ', evaluate_tree(expression_tree_root))
+# Error Cases:
+# evaluate_expression('4 (12E)')
+# evaluate_expression('4 (41)')
+# evaluate_expression('42+43**271')
